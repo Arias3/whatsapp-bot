c15ffbb1f8a5217b7bdd8019cfebced4e23cc4bf
Arias3 <juan43arias@gmail.com>
2025-12-16 17:52:47 -0500

    modified:   app.py

--- app.py
+++ app.py
@@ -50,3 +50,7 @@ def webhook():
             send_message(phone, "Hola 👋 Ya estoy funcionando.")
 
         return "ok", 200
+    
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
